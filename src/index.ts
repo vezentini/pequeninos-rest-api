cdd648737d26430a28bbcd8f064f5ddbb7e7617d
Gustavo Vezentini <gustavovezentini@frwk.com.br>
2024-03-13 09:43:37 -0300

    fix: add multer

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,37 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import loginAccount from './services/accounts.login.service'
 import upsertAccount from './services/accounts.upsert.service'
-import { setTimeout } from 'timers/promises'
 import mongoose from 'mongoose'
+import multer from 'multer';
 
 const PORT = process.env.PORT || 4000
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 const URI = "mongodb+srv://pequeninos-app:q8qDSmnSRxKkRWhl@cluster0.hhhy8qo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Bem-vindo!')
 })
 
-app.get('/loginAccount', async (req: Request, res: Response) => {
+const storage = multer.memoryStorage();
+
+const size = 150 * 1024 * 1024;
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: size,
+    fieldSize: size,
+  },
+});
+
+app.get('/loginAccount', upload.single('file'), async (req: Request, res: Response) => {
   const loginResult = await loginAccount(req.body)
   res.send(loginResult)
-})
+});
 
 app.post('/upsertAccount', async (req: Request, res: Response) => {
   const upsertResult = await upsertAccount(req.body)
--- src/services/accounts.login.service.ts
+++ src/services/accounts.login.service.ts
@@ -2,9 +2,10 @@ import { AccountLoginInput } from '../entities/inputs';
 import { Accounts, IAccountLogin } from '../entities/interfaces';
 
 const loginAccount = async (filter: AccountLoginInput): Promise<IAccountLogin> => {
-  if (!filter?.email || !filter?.password) return { success: false, account: null }
+  const { email, password } = filter;
+  if (!email || !password) return { success: false, account: null }
 
-  const accountDb = await Accounts.findOne({ ...filter });
+  const accountDb = await Accounts.findOne({ email, password });
 
   if (accountDb === null) return { success: false, account: null }
   return { success: true, account: accountDb };
