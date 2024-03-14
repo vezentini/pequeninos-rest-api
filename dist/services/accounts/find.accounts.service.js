c6ba985ea8283b64e99a275899d604f12a4283a3
Gustavo Vezentini <gustavovezentini@frwk.com.br>
2024-03-14 19:17:24 -0300

    feat: dist

--- dist/services/accounts/find.accounts.service.js
+++ dist/services/accounts/find.accounts.service.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const interfaces_1 = require("../../entities/interfaces");
-const ramda_1 = require("ramda");
 const findAccounts = async (input) => {
-    let filter = (0, ramda_1.isEmpty)(input === null || input === void 0 ? void 0 : input.id) ? {} : { id: input.id };
+    let filter = (input === null || input === void 0 ? void 0 : input.id) === null ? {} : { id: input.id };
+    console.log(filter);
     const accountDb = await interfaces_1.Accounts.find(filter);
     const accounts = accountDb.map((account) => ({
         id: account.id,
--- dist/services/classes/find.classes.service.js
+++ dist/services/classes/find.classes.service.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const interfaces_1 = require("../../entities/interfaces");
 const findClasses = async () => {
-    const accountDb = await interfaces_1.Classes.find();
-    const classes = accountDb.map((account) => ({
-        id: account.id,
-        name: account.name,
+    const classesDb = await interfaces_1.Classes.find();
+    const classes = classesDb.map((classObj) => ({
+        id: classObj.id,
+        name: classObj.name,
     }));
     return { classes };
 };
